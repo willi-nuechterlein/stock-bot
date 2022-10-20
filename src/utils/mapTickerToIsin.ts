ba913ea2c8b7153144916ac1a381f94354c96ab9
Willi <13342175+willi-nuechterlein@users.noreply.github.com>
2022-10-20 23:06:29 +0200

    add tickers

--- src/utils/mapTickerToIsin.ts
+++ src/utils/mapTickerToIsin.ts
@@ -1,5 +1,11 @@
 const isin: { [key: string]: string } = {
-  AMD: 'US0079031078'
+  AMD: 'US0079031078',
+  ADP: 'US0530151036',
+  UNP: 'US9078181081',
+  ADB: 'US00724F1012',
+  NFC: 'US64110L1061',
+  '6Z1': 'US0090661010',
+  'TL0': 'US88160R1014'
 }
 
 export default isin
